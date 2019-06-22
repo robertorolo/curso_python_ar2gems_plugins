2ef1dca3f0b50a7cebb3045f8e4d179858b0e8bd
Roberto Rolo <robertorolo@gmail.com>
2019-06-22 11:04:01 -0300

    final modifications

--- template_ar2gems_python.py
+++ template_ar2gems_python.py
@@ -2,6 +2,7 @@
 #################################################################################################
 
 #importe aqui os pacotes necessarios
+import sgems
 import numpy as np
 
 #################################################################################################
@@ -10,51 +11,51 @@ import numpy as np
 
 #exibe os valores do dicionario de parametros
 def read_params(a,j=''):
-  if j=='':
-    print("### Printing GUI parameters ###")
-  for i in a:
-    if (type(a[i])!=type({'a':1})):
-      print j+"['"+str(i)+"']="+str(a[i])+" type: "+str(type(a[i]))
-    else:
-      read_params(a[i],j+"['"+str(i)+"']")
+    if j=='':
+        print("### Printing GUI parameters ###")
+    for i in a:
+        if (type(a[i])!=type({'a':1})):
+            print j+"['"+str(i)+"']="+str(a[i])+" type: "+str(type(a[i]))
+        else:
+            read_params(a[i],j+"['"+str(i)+"']")
 
 #################################################################################################
 
 class template: #aqui vai o nome do plugin
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
 #################################################################################################
 
-	def initialize(self, params):
-		self.params = params
+    def initialize(self, params):
+        self.params = params
 		
-		#imprimindo o dicionario de parametros
-		print("dicionario de parametros: ", self.params)
+        #imprimindo o dicionario de parametros
+        print("dicionario de parametros: ", self.params)
 		
-		#executando a funcao exibe os valores do dicionario de parametros
-		read_params(params) #para nao printar comente essa linha
+        #executando a funcao exibe os valores do dicionario de parametros
+        read_params(params) #para nao printar comente essa linha
 		
-		return True
+        return True
 
 #################################################################################################
 
-	def execute(self):
-		
-		#aqui vai o codigo
-		print("Eu funciono") #apague essa linha
-		
-		return True
+    def execute(self):
+      
+        #aqui vai o codigo
+        print("Eu funciono") #apague essa linha
+
+        return True
 
 #################################################################################################
 	
-	def finalize(self):
-		return True
+    def finalize(self):
+        return True
 
-	def name(self):
-		return "template" #aqui vai o nome do plugin
+    def name(self):
+        return "template" #aqui vai o nome do plugin
 
 #################################################################################################
 
 def get_plugins():
-	return ["template"] #aqui vai o nome do plugin
+    return ["template"] #aqui vai o nome do plugin
